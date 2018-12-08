cac12688d1d4fa082675b7cadc0d538c9712a177
小池駿平 <shunpei42ba@gmail.com>
2018-12-09 05:47:01 +0900

    success amplify

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { AmplifyEventBus, AmplifyPlugin, components } from 'aws-amplify-vue'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
+
 Vue.config.productionTip = false
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,40 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="createTodo"></button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {API, graphqlOperation} from 'aws-amplify'
+import * as mutations from '@/graphql/mutations'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data: function () {
+    return {
+      input: {
+        // id: '1',
+        name: 'xxx',
+        description: '----'
+      }
+    }
+  },
+  methods: {
+    createTodo: function () {
+      API.graphql(graphqlOperation(mutations.createTodo, {input: this.input}))
+        .then((response) => {
+          console.info(response)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
   }
 }
 </script>
